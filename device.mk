5989f90d2768ab739b9bfc4e467cdc527f4f4a3a
simplicit <simplicit@olivierk.eu>
2016-01-30 01:24:06 +0100

    Build kernel from kernel source
    Change-Id: Id2f58a48ccc7393001510f5924fae119ceaa6b4d

--- device.mk
+++ device.mk
@@ -21,19 +21,12 @@ ifeq ($(wildcard vendor/nvidia/dragon-tlk/tlk),vendor/nvidia/dragon-tlk/tlk)
     SECURE_OS_BUILD ?= tlk
 endif
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/google/dragon-kernel/Image.fit
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
 LOCAL_FSTAB := $(LOCAL_PATH)/fstab.dragon
 TWRP_FSTAB := $(LOCAL_PATH)/twrp.fstab
 
 TARGET_RECOVERY_FSTAB = $(LOCAL_FSTAB)
 
 PRODUCT_COPY_FILES := \
-    $(LOCAL_KERNEL):kernel \
     $(LOCAL_PATH)/dump_bq25892.sh:system/bin/dump_bq25892.sh \
     $(LOCAL_PATH)/touchfwup.sh:system/bin/touchfwup.sh \
     $(LOCAL_PATH)/init.dragon.rc:root/init.dragon.rc \
